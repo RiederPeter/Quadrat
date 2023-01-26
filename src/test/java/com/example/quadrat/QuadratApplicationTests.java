72d19a01da07742de0130af150104b16a94f61c1
RiederPeter <32160760+RiederPeter@users.noreply.github.com>
2023-01-26 18:00:36 +0100

    Initial commit

---
+++ src/test/java/com/example/quadrat/QuadratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quadrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuadratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
